ce8a045fe9b1c2e0918432050e6dff6d35471a79
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-02-28 18:56:30 +0200

    Create Book-constructor and add books from cli

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 218, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 234, false);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 432, true);
+
+console.log(myLibrary);
